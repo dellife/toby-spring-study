e9a4ff2d5476b1affcaaf0ead0b98192390376ec
dellife <seheekwon21@gmail.com>
2020-04-18 20:25:01 +0900

    2-12 get() 테스트 기능을 보완한 addAndGet() 테스트

--- src/test/java/com/dellife/springbook/user/dao/UserDaoTest2.java
+++ src/test/java/com/dellife/springbook/user/dao/UserDaoTest2.java
@@ -17,21 +17,24 @@ class UserDaoTest2 {
 
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        User user1 = new User("sehee", "aaa", "asdfasdf");
+        User user2 = new User("dellife", "bbb", "asdfasdf");
+
         dao.deleteAll();
         assertThat(dao.getCount()).isEqualTo(0);
 
-        User user = new User();
-        user.setId("user");
-        user.setName("sehee");
-        user.setPassword("password");
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount()).isEqualTo(2);
 
-        dao.add(user);
-        assertThat(dao.getCount()).isEqualTo(1);
+        User userget1 = dao.get(user1.getId());
+        User userget2 = dao.get(user2.getId());
 
-        User user2 = dao.get(user.getId());
+        assertThat(userget1.getName()).isEqualTo(user1.getName());
+        assertThat(userget1.getPassword()).isEqualTo(user1.getPassword());
 
-        assertThat(user2.getName()).isEqualTo(user.getName());
-        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+        assertThat(userget2.getName()).isEqualTo(user2.getName());
+        assertThat(userget2.getPassword()).isEqualTo(user2.getPassword());
 
     }
 
