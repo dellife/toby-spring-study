104a8f455a6c9ea170f7c32732126f4a363f96b6
dellife <seheekwon21@gmail.com>
2020-04-20 23:53:13 +0900

    3-3 JDBC 예외처리를 적용한 getCount() 메소드

--- src/main/java/com/dellife/springbook/user/dao/UserDao.java
+++ src/main/java/com/dellife/springbook/user/dao/UserDao.java
@@ -91,17 +91,39 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
 
-        PreparedStatement ps = c.prepareStatement("select count(1) from users");
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        ResultSet rs = ps.executeQuery();
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(1) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
 
-        rs.next();
-        int count = rs.getInt(1);
-        rs.close();
-        ps.close();
-        c.close();
-        return count;
+                }
+            }
+        }
     }
 }
