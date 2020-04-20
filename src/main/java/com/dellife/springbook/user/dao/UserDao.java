ed896400098aefcff87d2283a360803f86457089
dellife <seheekwon21@gmail.com>
2020-04-21 00:20:10 +0900

    3-15 user 정보를 AddStatement에 전달해주는 add() 메소드

--- src/main/java/com/dellife/springbook/user/dao/UserDao.java
+++ src/main/java/com/dellife/springbook/user/dao/UserDao.java
@@ -24,17 +24,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException, EmptyResultDataAccessException {
@@ -64,32 +55,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            StatementStrategy strategy = new DeleteAllStatement();
-            jdbcContextWithStatementStrategy(strategy);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-
-                }
-            }
-        }
+        StatementStrategy strategy = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(strategy);
     }
 
     public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
