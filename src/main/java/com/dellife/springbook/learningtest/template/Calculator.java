7af9c433c22d7707ad051fdc1dbdbbe05f3c4f16
dellife <seheekwon21@gmail.com>
2020-04-25 21:36:19 +0900

    3-40 라인별 작업 정의한 LineCallback인터페이스의 사용, 테스트 추가

--- src/main/java/com/dellife/springbook/learningtest/template/Calculator.java
+++ src/main/java/com/dellife/springbook/learningtest/template/Calculator.java
@@ -7,22 +7,51 @@ import java.io.IOException;
 public class Calculator {
 
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallback sumCallback =
-                new BufferedReaderCallback() {
+        LineCallback sumCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSometingWithReader(BufferedReader br) throws IOException {
-                        Integer sum = 0;
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            sum += Integer.valueOf(line);
-                        }
-
-                        return sum;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value + Integer.valueOf(line);
                     }
                 };
+        return lineReadTemplate(filepath, sumCallback, 0);
 
-        return fileReadTemplate(filepath, sumCallback);
+    }
 
+    public int calcMutiply(String filepath) throws IOException {
+        LineCallback multiplyCallback =
+                new LineCallback() {
+                    @Override
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value * Integer.valueOf(line);
+                    }
+                };
+        return lineReadTemplate(filepath, multiplyCallback, 1);
+    }
+
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+
+            return res;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
@@ -44,22 +73,5 @@ public class Calculator {
             }
         }
 
-    }
-
-    public int calcMutiply(String numFilepath) throws IOException {
-        BufferedReaderCallback multiplyCallbafck =
-                new BufferedReaderCallback() {
-                    @Override
-                    public Integer doSometingWithReader(BufferedReader br) throws IOException {
-                        Integer multiply = 1;
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            multiply *= Integer.valueOf(line);
-                        }
-
-                        return multiply;
-                    }
-                };
-        return fileReadTemplate(numFilepath, multiplyCallbafck);
     }
 }
---
+++ src/main/java/com/dellife/springbook/learningtest/template/LineCallback.java
@@ -0,0 +1,5 @@
+package com.dellife.springbook.learningtest.template;
+
+public interface LineCallback {
+    Integer doSomethingWithLine(String line, Integer value);
+}
